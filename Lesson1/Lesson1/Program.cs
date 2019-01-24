0ac157b95877a8d03216d911aa462be638ef4c37
Aleksandr Konovalov <a.konovalov@dodopizza.com>
2019-01-24 16:16:05 +0300

    Add profile

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -6,7 +6,25 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+       
+           Console.WriteLine("Введите имя");
+           string name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            int growth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите вес");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Вас зовут " + name + $" ,ваша фамилия {surname} и вам {age} годиков, а выросли вы до {growth:F} сантиметров при этом весите {weight: #.####} килограмм");
+
+            Console.ReadKey();
         }
     }
 }
