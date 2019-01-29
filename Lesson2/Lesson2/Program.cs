e31ec5b9fc685fb41caf45978a36d43d6dbd0489
Aleksandr Konovalov <a.konovalov@dodopizza.com>
2019-01-29 20:14:38 +0300

    Task 5

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -28,13 +28,16 @@ namespace Lesson2
 
             CounOfOddNumbers();
 
-//            Задание 4
-//            Реализовать метод проверки логина и пароля. На вход подается логин и пароль.
-//            На выходе истина, если прошел авторизацию, и ложь, если не прошел
-//            (Логин: root, Password: GeekBrains). Используя метод проверки логина и
-//            пароля, написать программу: пользователь вводит логин и пароль, программа
-//            пропускает его дальше или не пропускает. С помощью цикла do while ограничить
-//            ввод пароля тремя попытками.
+
+            #region Task4
+
+////            Задание 4
+////            Реализовать метод проверки логина и пароля. На вход подается логин и пароль.
+////            На выходе истина, если прошел авторизацию, и ложь, если не прошел
+////            (Логин: root, Password: GeekBrains). Используя метод проверки логина и
+////            пароля, написать программу: пользователь вводит логин и пароль, программа
+////            пропускает его дальше или не пропускает. С помощью цикла do while ограничить
+////            ввод пароля тремя попытками.
 
             int CoutOfAttempts = 3;
             do
@@ -48,6 +51,22 @@ namespace Lesson2
                 CoutOfAttempts--;
 
             } while (CoutOfAttempts > 0);
+
+            #endregion
+
+            #region Task5
+
+//            Задание 5
+//             а) Написать программу, которая запрашивает массу и рост человека,
+//                вычисляет его индекс массы и сообщает, нужно ли человеку
+//                похудеть, набрать вес или все в норме;
+//
+//            б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+
+            BodyMassIndexCheck();
+
+
+            #endregion
 
         }
 
@@ -123,6 +142,59 @@ namespace Lesson2
             }
             return false;
         }
+
+        /// <summary>
+        /// Вычисляет ИМТ и говорит сколько сбросить или набрать
+        /// </summary>
+        public static void BodyMassIndexCheck()
+        {
+            Console.WriteLine("Введите свою массу");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите свой рост");
+            double growth = Convert.ToDouble(Console.ReadLine());
+            double massDifference = 0;
+
+            do
+            {
+
+                double BodyMassIndex = weight/Math.Pow((growth/100),2) ;
+
+                if (BodyMassIndex <= 18.5)
+                {
+                    massDifference += 1;
+                    weight += 1;
+                }
+
+                else if (BodyMassIndex <= 25 )
+                {
+                    if (massDifference == 0)
+                    {
+                        Console.WriteLine("Нормальная масса тела");
+                        break;
+                    }
+
+                    if (massDifference > 0)
+                    {
+                        Console.WriteLine($"Недостаточная (дефицит) масса тела, нужно набрать {massDifference} кг.");
+                        break;
+                    }
+
+                    Console.WriteLine($"Избыточная масса тела, нужно сбросить {-massDifference} кг.");
+                    break;
+
+                }
+
+                else
+                {
+                    massDifference -= 1;
+                    weight -= 1;
+                }
+
+            } while (true);
+
+
+        }
+
 
     }
 }
